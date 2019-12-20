7ee346d877bcdf5669d2d2c599001a3181cdfe87
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-20 10:40:12 -0600

    working on credentials

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+import axiosWithAuth from './axiosWithAuth';
+import { render } from "react-dom";
+
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Bubble App!</h1>
+      </div>
+    )
+  }
 };
 
 export default Login;
